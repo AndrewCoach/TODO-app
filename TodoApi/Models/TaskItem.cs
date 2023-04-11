c078169379a3bb60faa2c85d7e99533c96b3df52
Andrew Sharp <kociandreas1@gmail.com>
2023-04-11 14:38:28 +0200

    Project setup

---
+++ TodoApi/Models/TaskItem.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Priority { get; set; }
+
+        [Required]
+        public TaskStatus Status { get; set; }
+    }
+
+    public enum TaskStatus
+    {
+        Initial,
+        InProgress,
+        Completed
+    }
+}
+
